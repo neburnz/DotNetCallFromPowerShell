47e140e5d72950c172265d508f261035b09f263d
Rubén Guzmán Jiménez <rubeng@extend.com.mx>
2018-03-12 17:36:29 -0600

    Added example to call instance and class methods from PowerShell

---
+++ DotNetClassLibrary/DotNetClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetClassLibrary
+{
+    public class DotNetClass
+    {
+        public DotNetClass()
+        {
+
+        }
+
+        public DateTime CallMethodWithoutArguments()
+        {
+            return DateTime.UtcNow;
+        }
+
+        public static string CallStaticMethod(string name, int age)
+        {
+            return string.Format("Hello {0}!... Your age is {1}", name, age);
+        }
+    }
+}
---
+++ PowerShellScript/Script.ps1
@@ -0,0 +1,19 @@
+﻿# Load the base path from script location.
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+# Build the assembly path.
+$assemblyPath = $scriptPath + '\DotNetClassLibrary.dll'
+
+# Loading the assembly.
+[Reflection.Assembly]::LoadFile($assemblyPath)
+
+# Creating a new class instance.
+$instance = New-Object DotNetClassLibrary.DotNetClass
+
+# Executing a simple insance method.
+$date = $instance.CallMethodWithoutArguments()
+Write-Output $date
+
+# Executing a class method.
+$name = [DotNetClassLibrary.DotNetClass]::CallStaticMethod("neburnz",36)
+Write-Output $name
